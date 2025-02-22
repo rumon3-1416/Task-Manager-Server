7fb1460e85ea420ca30096b830474121857c01da
s-h-rumon <shahidhasanruman3@gmil.com>
2025-02-22 12:16:12 +0600

    reordering cat done

--- controllers/putController.js
+++ controllers/putController.js
@@ -81,10 +81,28 @@ const sameReorder = tryCatch(async (req, res) => {
 // Update diff Category Order
 const catReorder = tryCatch(async (req, res) => {
   const { id } = req.params;
-  const tasks = req.body;
-  console.log(id, tasks);
+  const { source, destination } = req.body;
 
-  res.send({ ms: 'hi' });
+  const projectsCol = await connectDB('projects');
+
+  const result1 = await projectsCol.updateOne(
+    { _id: new ObjectId(id), 'task_categories.category': source.category },
+    {
+      $set: {
+        'task_categories.$.tasks': source.tasks,
+      },
+    }
+  );
+  const result2 = await projectsCol.updateOne(
+    { _id: new ObjectId(id), 'task_categories.category': destination.category },
+    {
+      $set: {
+        'task_categories.$.tasks': destination.tasks,
+      },
+    }
+  );
+
+  res.send({ result1, result2 });
 });
 
 module.exports = {
